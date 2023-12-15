ce2ea0cba4f412aaf16ddba0c184e0fd853c5f15
Jaeyoon <wamapuck4270@gmail.com>
2023-12-15 15:48:53 +0900

    단어들 중에서 5개의 빈칸이다 테스트 완

--- real_hangman_game_ver.py
+++ real_hangman_game_ver.py
@@ -13,29 +13,29 @@ for tag in result.find_all('li'):
 
 joined_sentenceList = '.'.join(sentenceList)
 
-print(joined_sentenceList)
-print(type(joined_sentenceList))
+#print(joined_sentenceList)
+#print(type(joined_sentenceList))
 
 word_token_Full = nltk.word_tokenize(joined_sentenceList)
-print(type(word_token_Full))
-print(word_token_Full)
+#print(type(word_token_Full))
+#print(word_token_Full)
 
 lowered_word_token_Full = []
 
 for i in word_token_Full:
     lowered_word_token_Full.append(i.lower())
     
-print(lowered_word_token_Full)
+#print(lowered_word_token_Full)
 
 tagged_lowered_word_token_Full = nltk.pos_tag(lowered_word_token_Full)
 
-print(tagged_lowered_word_token_Full)
+#print(tagged_lowered_word_token_Full)
 
 Noun_words = []
 for word, pos in tagged_lowered_word_token_Full:
     if 'NN' in pos:
         Noun_words.append(word)
-print(Noun_words)
+#print(Noun_words)
 
 wlem = nltk.WordNetLemmatizer()
 lemmatized_words = []
@@ -43,13 +43,30 @@ for word in Noun_words:
     new_word = wlem.lemmatize(word)
     lemmatized_words.append(new_word)
 
-print(lemmatized_words)
+#print(lemmatized_words)
 
 from collections import Counter
 c = Counter(lemmatized_words)
-print(c)
+#print(c)
 k = 20
-print(c.most_common(k))
+#print(c.most_common(k))
+
+
+print(Noun_words)
+
+considerable_words = []
+
+count_of_blanks = int(input("몇개의 빈칸으로 이루어져 있나요?"))
+
+for word in Noun_words:
+     if len(word) == count_of_blanks:
+         considerable_words.append(word)
+         
+         
+print(considerable_words)
+
+
+
 
 # 대충 직접 컴퓨터가 맞추는형태가 아니라 우리가 행맨게임을 가장 빠르게 우승하도록 도와주는 코드
 # 단어들 중에서 5개의 빈칸이다
